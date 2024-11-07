53a26a5709e17c257d17ce4fe1dcf878248ce7e1
tonyngoo <tngo07@uoguelph.ca>
2024-11-07 16:07:08 -0500

    upload changes for PlayerStateModule

--- PlayerStateModule.rb
+++ PlayerStateModule.rb
@@ -1,6 +1,8 @@
 require_relative "Board"
 require_relative "PlayerPhase"
 require_relative "MoveColor"
+
+# This class is responsible for managing the player's state and phase
 class PlayerStateModule
     
     attr_accessor :currentBlackPhase, :currentWhitePhase, :currentTurnColor, :boardInstance
@@ -13,18 +15,42 @@ class PlayerStateModule
     end
   
     def UpdatePlayerPhase
-      # Update player phase based on number of pieces on board
+      total_black_pieces = @boardInstance.TotalBlackPieces
+      total_white_pieces = @boardInstance.TotalWhitePieces
+
+      if @currentTurnColor == MoveColor::BLACK
+        if total_black_pieces <= 3 && @currentBlackPhase == PlayerPhase::MOVING
+          change_state(PlayerPhase::FLYING)
+        elsif total_black_pieces == 9 && @currentBlackPhase == PlayerPhase::PLACING
+          change_state(PlayerPhase::MOVING)
+        end
+      elsif @currentTurnColor == MoveColor::WHITE
+        if total_white_pieces <= 3 && @currentWhitePhase == PlayerPhase::MOVING
+          change_state(PlayerPhase::FLYING)
+        elsif total_white_pieces == 9 && @currentWhitePhase == PlayerPhase::PLACING
+          change_state(PlayerPhase::MOVING)
+        end
+      end
+    end
+        
     end
   
     def checkState
       # Retrieves and returns current player phase and state
+      if @currentTurnColor == MoveColor::BLACK
+        { phase: @currentBlackPhase, color: MoveColor::BLACK }
+      else
+        { phase: @currentWhitePhase, color: MoveColor::WHITE }
+      end
     end
   
     def changeState(state)
       # Change the player phase or state
+      if @currentTurnColor == MoveColor::BLACK
+        @currentBlackPhase = state
+      else
+        @currentWhitePhase = state
+      end
     end
   end
   
-
-
-  #test
