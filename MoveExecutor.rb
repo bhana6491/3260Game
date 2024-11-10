fbd0087540426c189fb032303fca9626f3510bc5
lee-ranaweer <lee.ranaweer@gmail.com>
2024-11-10 11:24:46 -0500

    MoveExecutor class completed

--- MoveExecutor.rb
+++ MoveExecutor.rb
@@ -1,27 +1,55 @@
 class MoveExecutor
   attr_accessor :player_state, :Move_colour, :From_coordinate, :To_coordinate, :Board
 
-  def initialize(board)
+  def initialize(board, player_state_module)
     @Board = board
-
-
   end
 
-  def isPlayerMoveValid(from_coordinate, to_coordinate, player_state, move_colour)
-    # Validates if the player move is executable
-  end
+  def sendCoordinate(from_coordinate, to_coordinate)
+    game_state = askForState
 
-  def makeMove(from_coordinate, to_coordinate, player_state)
-    # Executes the move if validated
+    @From_coordinate = from_coordinate
+    @To_coordinate = to_coordinate
+    @player_state = game_state[:phase]
+    @Move_colour = game_state[:color]
+
+    valid = isPlayerMoveValid(from_coordinate, to_coordinate, @player_state[:phase], @player_state[:color])
+
+    if valid
+      makeMove(from_coordinate, to_coordinate, @player_state)
+    else
+      puts "Invalid move" 
+    end
   end
 
   private
 
   def askForState
-    # Requests the player state
+    player_state_module.checkState
   end
   
-  def processMove
-    # Executes the move process
+  def makeMove(from_coordinate, to_coordinate, player_state)
+    board.ManipulateBoard(player_state[:color], player_state[:phase], from_coordinate, to_coordinate)
+  end
+
+  def isPlayerMoveValid(from_coordinate, to_coordinate, player_state, move_colour)
+    # The method retrieves the move subset array from the board instance variable
+    valid_moves = board.CalculatePossibleMoves(@From_coordinate)
+
+    # The method searches the move subset for from_coordinate and to_coordinate as a possible move
+    if valid_moves.include?(@To_coordinate)
+      return true
+    else
+      return false
+    end
   end
 end
+
+# Change: Initialized it with an instance of the PlayerStateModule so it can ask for state
+# Assumption: Called the CalculatePossibleMoves method with one argument, from_coordinate
+# Incomplete: sendCoordinate puts "Invalid move" if isPlayerMoveValid returns false 
+# Additon: Added public method sendCoordinate which was mentioned in sequence diagrams
+# Assumption: Removed processMove as it is not clear what purpose it would serve
+# Incomplete: May need to remove a lot of the instance variables as they are unnecessary 
+# Assumption: Changed MakeMove to be private, as it is only called by sendCoordinate
+# Assumption: changed isPlayerMoveValid to be private, as it is only called by sendCoordinate
